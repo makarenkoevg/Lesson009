cc33975a72b6e64e8451b608e36c457a61e77bf9
makarenkoevg <makarenkoevg@mail.ru>
2022-12-20 03:30:08 +0500

    66

---
+++ 66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using System;
+using static System.Console;
+
+Clear();
+
+
+Write("Введите целое полижительное M: ");
+int m = int.Parse(ReadLine());
+
+Write("Введите целое полижительное N: ");
+int n = int.Parse(ReadLine());
+
+
+int Summ(int a, int b)
+{
+    if (b==a)
+    {
+        return a;
+    }
+    return (Summ(a,b-1) + b);
+}
+
+if (n>m){
+    WriteLine(Summ(m,n));
+}
